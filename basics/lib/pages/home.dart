ac3a4fa87a17f6bf65b927b01fb2f53aa3290285
Salim Rutaganda <rutagandasalim@gmail.com>
2024-10-15 13:20:00 +0200

    added fonts

--- basics/lib/pages/home.dart
+++ basics/lib/pages/home.dart
@@ -1,2 +1,31 @@
 import 'package:flutter/material.dart';
 
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:  const Text(
+          'Breakfast',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          )
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        centerTitle: true,
+        leading: Container(
+        margin: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(10),
+          )
+        ),
+      ),
+    );
+  }
+}
